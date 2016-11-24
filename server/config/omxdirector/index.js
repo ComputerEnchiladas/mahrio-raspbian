2240825bb4fb91d8ab4d2c91a0664c7763d3cdb7
JesusGuerrero <rocha.jesus@hotmail.com>
2016-11-23 16:30:00 -0800

    Adding ability to play next and previous when playing all

--- server/config/omxdirector/index.js
+++ server/config/omxdirector/index.js
@@ -2,4 +2,15 @@
 
 var omx = require('omxdirector');
 
+omx.on('load', function(files, options) {
+
+});
+
+omx.on('stop', function(){
+
+});
+omx.on('next', function(file){
+
+});
+
 module.exports = omx;
